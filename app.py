9a6a6eac9ecdb9de684c3f768765ada7b9dc2675
npaltiel <nochum.paltiel@gmail.com>
2025-03-10 14:36:29 -0400

    Added logout button

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, session, send_file
 from markupsafe import Markup
-import base64
+import datetime
 import os
 from routes.caregiver_team import caregiver_team_bp
 from routes.soc import soc_bp
@@ -15,6 +15,17 @@ app.register_blueprint(caregiver_team_bp, url_prefix="/caregiver_team")
 app.register_blueprint(soc_bp, url_prefix="/soc")
 
 PASSWORD = os.getenv("APP_PASSWORD")
+
+@app.before_request
+def check_session_timeout():
+    """Logout user if inactive for too long."""
+    if 'logged_in' in session:
+        last_activity = session.get('last_activity')
+        if last_activity and (datetime.datetime.now() - last_activity).total_seconds() > 1800:
+            session.clear()  # Log out user
+            return redirect(url_for('login'))
+        session['last_activity'] = datetime.datetime.now()  # Update last activity timestamp
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
