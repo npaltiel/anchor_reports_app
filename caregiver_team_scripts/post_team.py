55528def3443954cf682efe7326d1c5c4a2da406
npaltiel <nochum.paltiel@gmail.com>
2025-04-29 12:41:44 -0400

    Fixed check for first work date

--- caregiver_team_scripts/post_team.py
+++ caregiver_team_scripts/post_team.py
@@ -22,7 +22,7 @@ def get_employment_types(caregiver, add_hcss=False, remove_hcss=False):
     if add_hcss and 'HCSS' not in types:
         types.append('HCSS')
 
-    if remove_hcss and first_work_date and 'HCSS' in types:
+    if remove_hcss and not first_work_date and 'HCSS' in types:
         types.remove('HCSS')
 
     res = ''
