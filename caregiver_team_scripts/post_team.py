8fcd19b2aef6581e9ba8fbbf86089e4cb0c51305
npaltiel <nochum.paltiel@gmail.com>
2025-04-29 13:06:09 -0400

    fixed print

--- caregiver_team_scripts/post_team.py
+++ caregiver_team_scripts/post_team.py
@@ -8,17 +8,14 @@ app_name = os.getenv("APP_NAME")
 api_secret = os.getenv("API_SECRET")  
 api_key = os.getenv("API_KEY")
 
-app_name = 'ANTH1476'
-api_secret = '8d2a50da-b799-4ce1-b780-920203e2edec'
-api_key = 'MQAwADIANgA2ADgALQBEAEMANABCADAARQAzADMANgAzAEQANgBDADgARgBFADIAMwA2ADMANQA3AEUAMQA2ADgAOQBBAEQAMQA='
-
-
 def get_employment_types(caregiver, add_hcss=False, remove_hcss=False):
     segment_start = '<Discipline>'
     segment_end = '</Discipline>\n'
     types = caregiver['Employment Type'].split(', ')
     first_work_date = caregiver['First Work Date']
-    print('first work date: ' + first_work_date)
+    if first_work_date:
+      print('first work date: ' + first_work_date)
+    print('False Check')
     print(first_work_date == False)
        
     if add_hcss and 'HCSS' not in types:
